ec375e79f7c2976b509b8806d8e1a205a2e6143a
Tommy Yang <tommy.yang@freshprep.ca>
2020-03-31 09:23:42 -0700

    made changes to only allowing Richmond

--- app/models/postal.rb
+++ app/models/postal.rb
@@ -1,4 +1,35 @@
 class Postal < ApplicationRecord
 
-validates :postalcode, presence: true , length: {minimum: 6, maximum: 6},  format: { with: /\A^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$\z/}
+     validates :postalcode, presence: true, length: {minimum: 6, maximum: 6},  format: { with: /\A^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$\z/}
+
+
+     validate :code_starts_with 
+
+     private 
+
+    def code_starts_with
+       if postalcode.start_with?("V7C", "V7B", "V6V", "V7E", "V6Y", "V7A", "V6X", "V6W")
+
+       elsif 
+       errors.add(:postalcode, "does not fall into Richmond")
+       end
+    end
+
+     
+#validates_inclusion_of :postalcode, :in => ["V7C", "V7B"]
+  
+    #validates_each :postalcode do  |model, attr, value|
+         #a = "V7C"
+        # b= "V7B"
+         #c= "V6V"
+        #if  value[0..2] != (a or b or c)
+          # model.errors.add(attr, 'does not belong in Richmond')
+     # end
+    #end
+    
+
+#validates :postalcode, presence: true, if: -> { postalcode = "V7C2M2"} 
+
+
+
 end
