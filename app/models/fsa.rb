ed771a1e5c0a2c28ee7e43c9ceb63ab3e65fca20
Tommy Yang <tommy.yang@freshprep.ca>
2020-04-08 11:13:32 -0700

    fsa constraints

--- app/models/fsa.rb
+++ app/models/fsa.rb
@@ -1,3 +1,14 @@
 class Fsa < ApplicationRecord
+   
+   
+   
+    validates :fsacode, presence: true, length: {minimum: 3, maximum: 3},  format: { with: /\A^(?!.*[DFIOQU])[A-VXY][0-9][A-Z]$\z/}
+   
+    validates_uniqueness_of :fsacode
+   
+   
     has_many :postals
+
+
+
 end
