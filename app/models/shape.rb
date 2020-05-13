f670d4ef3fed86923410f46d1adb8c2d2c854f50
Tommy Yang <tommy.yang@freshprep.ca>
2020-05-13 09:25:03 -0700

    made changes from sqlite3 to postgres

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,14 +1,14 @@
 class Shape < ApplicationRecord
 
     validates :fsa, presence: true
-    validates :btm_left_x, presence: true, numericality: {only_integer: true} 
-    validates :btm_left_y, presence: true, numericality: {only_integer: true} 
-    validates :btm_right_x, presence: true, numericality: {only_integer: true} 
-    validates :btm_right_y, presence: true, numericality: {only_integer: true} 
-    validates :top_left_x, presence: true, numericality: {only_integer: true} 
-    validates :top_left_y, presence: true, numericality: {only_integer: true} 
-    validates :top_right_x, presence: true, numericality: {only_integer: true} 
-    validates :top_right_y, presence: true, numericality: {only_integer: true} 
-    
+    validates :btm_left_x, presence: true
+    validates :btm_left_y, presence: true 
+    validates :btm_right_x, presence: true
+    validates :btm_right_y, presence: true
+    validates :top_left_x, presence: true
+    validates :top_left_y, presence: true
+    validates :top_right_x, presence: true
+    validates :top_right_y, presence: true
+
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_11_184200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "autocompletes", force: :cascade do |t|
     t.string "address"
     t.integer "street_number"
